59bffdd4dc72fc1a663ed7b59f2a515a816de17a
Octavio <octaviolimasan@gmail.com>
2026-07-21 21:12:00 -0300

    Added basic logic to create a file to the vault

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from app.vault import get_default_vault
+from os import mkdir
+from pathlib import Path
+
+
 def main():
-    print("The Beginning!")
+    vault = get_default_vault()
+    title = ""
+
+    while title == "":
+        title = input("Enter the markdown file title: ")
+
+    with open(Path(vault.path, title + ".md"), "w", encoding="utf-8") as f:
+        f.write("a")
 
 
 if __name__ == "__main__":
